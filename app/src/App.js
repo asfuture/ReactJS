1dedca411845d9bb0dafb083a661ea4004c6ce8b
Alex Ferreira <asfuture2019@gmail.com>
2022-07-05 22:23:22 -0300

    Primeiras function criadas

--- app/src/App.js
+++ app/src/App.js
@@ -1,22 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+function sum(a,b){
+  return a+b;
+}
+
+function primeiroJSX(){
+  return (
+    <div>
+      Alex Ferreira - Introdução ao Reactjs
+     <h3>Nota : {sum(5,5)}</h3> 
+    </div>
+  );
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Olá! Mundo. React
         </p>
+        {primeiroJSX()}
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://asfuture.com.br"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          ASFuture
         </a>
+        
       </header>
     </div>
   );
